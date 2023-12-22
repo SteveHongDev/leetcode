b58934259926c90ecdd9f7859c49dd92406f7947
Seongbeom Hong <sb12@tuta.io>
2023-12-22 09:32:23 +0900

    Time: 193 ms (25.00%), Space: 15.8 MB (25.00%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.swift
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.swift
@@ -3,19 +3,14 @@ class Solution {
         var left = Substring()
         var right = Substring()
         var result = 0
-        
-        if (s.count == 2) {
-            result = (s.first == "0" ? 1 : 0 ) + (s.last == "1" ? 1 : 0)
-        } else {
-            for i in 1 ..< s.count {
+        for i in 1 ..< s.count {
             left = s.prefix(i)
             right = s.suffix(s.count - i)
-            
+
             let tmp = left.filter { $0 == "0" }.count + right.filter { $0 == "1" }.count
             if tmp > result {
                 result = tmp
             }
-        }
         }
         
         return result
