d67bfb72f0147bccc4f185f9e4c37cfe5342ddb2
Seongbeom Hong <sb12@tuta.io>
2024-01-01 19:03:11 +0900

    Time: 375 ms (8.33%), Space: 16.1 MB (12.50%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func findContentChildren(_ g: [Int], _ s: [Int]) -> Int {
+        var gHead = 0
+        var sHead = 0
+        var result = 0
+        
+        var gSort = g.sorted()
+        var sSort = s.sorted()
+
+        while gHead != g.count && sHead != s.count {
+            if gSort[gHead] <= sSort[sHead] {
+                gHead += 1
+                sHead += 1
+                result += 1
+            } else {
+                sHead += 1
+            }
+        }
+
+        return result
+    }
+}
