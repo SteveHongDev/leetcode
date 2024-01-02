c5aa1b0ce75596745bf9e7c118c493b59dd8f7da
Seongbeom Hong <stevehonggg@gmail.com>
2024-01-02 18:46:57 +0900

    Time: 61 ms (5.29%), Space: 15.1 MB (14.58%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.swift
+++ 0009-palindrome-number/0009-palindrome-number.swift
@@ -4,8 +4,7 @@ class Solution {
             return false
         } else {
             let str = "\(x)"
-            let length = str.count
-            for i in 0 ..< length / 2 {
+            for i in 0 ..< str.count / 2 {
                 if str[str.index(str.startIndex, offsetBy: i)] == str[str.index(str.endIndex, offsetBy: -1 - i)] {
                     continue
                 } else {
